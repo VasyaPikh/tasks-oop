8a36f8d172ae35fd56b9ca08ca2ffb8e84507198
VasyaPikh <pikhvasya@gmail.com>
2021-10-29 15:13:01 +0000

    Task2

--- task2/task2.js
+++ task2/task2.js
@@ -1,27 +1,27 @@
 class Person {
     constructor(name, surname){
-        this.name = name;
-        this.surname = surname;
+        this._name = name;
+        this._surname = surname;
     }
     showFullName (){
-        console.log (`${this.surname} ${this.name}`);
+        return `${this._name} ${this._surname}`
     }
 }
-
 
 class Student extends Person {
     constructor (name, surname, year) {
         super(name, surname)
-        this.year = year;
+        this._year = year;
     }
-    showFullName(middLeNAme){
-        return `${this.surname} ${this.name} ${this.middLeNAme}`;
+    showFullName(middLeName){
+        return `${this._name} ${this._surname} ${this.middLeName}`;
     }
     showCourse (){
-        return new Data().getFullYear() - this.year; 
+        let data = new Date();
+        return data.getFullYear() - this._year; 
     }
 }
 
-const stud1 = new Student('Petro', 'Petrenko', '2017');
+const stud1 = new Student("Petro", "Petrenko", 2017);
 console.log(stud1.showFullName("Petrovych"));
 console.log("Current course:" + stud1.showCourse());
