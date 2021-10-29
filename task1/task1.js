6a241e2cd77aa0faf29981b6114e34d7a52078ad
VasyaPikh <pikhvasya@gmail.com>
2021-10-29 14:51:49 +0000

    Task1

--- task1/task1.js
+++ task1/task1.js
@@ -1,10 +1,24 @@
-function student(firstName, lastName, direction){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.direction = direction;
-        this.showFullName = function() {
-           return this.firstName + " " + this.lastName;
-        	  }
+class Student {
+    constructor(fullname, direction) {
+        this._fullname = fullname;
+        this._direction = direction;
+    }
+
+    showFullName(){
+        let result = this._fullName;
+        return result;
+    }
+    nameIncludes(data) {
+        if (this.showFullName().includes(data)) return console.log (true);
+        return console.log(false);
+    }
+    static studentBuilder(fullName, direction){
+        return new Student (fullName, direction);
+    }
 }
-let stud1 = new student("Ivan", "Petrenko", "web");
-let stud2 = new student("Sergiy", "Koval", "python");
+    const stud1 = new Student ("Ivan Petrenko", "web");
+    const stud2 = new Student ("Sergiy Koval", "puthon");
+    const stud3 = Student.studentBuilder ("Ihor Kohut", "qc");
+
+    stud1.nameIncludes('Ivan'); 
+    stud1.nameIncludes('Denysenko');
